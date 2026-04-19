f9ec22ef3afa958c550098af93649a4502abfc3c
siyu <16973015+siyujia@user.noreply.gitee.com>
2026-04-19 16:25:27 +0800

    Change router to hash mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Analyze from "./pages/Analyze";
 import Graph from "./pages/Graph";
